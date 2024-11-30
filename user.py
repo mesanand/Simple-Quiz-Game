3c92f33c840d09a7d79a563e887d585ea32eac78
Mehr Anand <mehr.s.anand@gmail.com>
2024-11-30 16:49:40 -0500

    First run
    Actually able to get the code running! Have the basic questions and answers

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 #make the questions
 questions = [
-("What school do I go to", "Northeastern"),("Who is the senior associate Dean of Khoury","Ben Hescot"),
+("What school do I go to", "Northeastern"),("Who is the senior associate Dean of Khoury","Ben Hescott"),
 ("What is my major", "computer science"), ("Where do I live", "New York")
 ]
 #make a nice intro to the user
 name = input("What is your name? ")
 print(f"Hello, {name}")
-#Prints out question
+# Prints out question
 # gives you choices of A,B,C,D (edge case can be uppercase or lower case and spacing does not matter either)
 def playQuiz(): 
     #Has to start with zero points and the first question, wth is question 0 
@@ -16,4 +16,17 @@ def playQuiz():
     
     for question in questions: 
         print(f"{questionNum} : {question[0]}") #Just push out the question 
+        answer = input("YOU PUT:" ).strip() #you gotta find out how to make it so that if they put 
+        #an answer weird - the program can still take it (like edge cases) 
         
+        if answer.lower() == question[1].lower():
+            score = score + 1 #they got it right!
+        else: 
+            score = score #They got it wrong!
+            print("nu uh")
+        questionNum = questionNum + 1 #move to next question
+        
+    
+    print(f"ur final score is: ", score, "out of: ", len(questions)) 
+playQuiz()
+            
