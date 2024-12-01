0cb617b0e489b01fac42423d21fc9b1798c94e30
Mehr Anand <mehr.s.anand@gmail.com>
2024-11-30 19:04:14 -0500

    Final Draft of the project!
    Make multiple tuples, and showed user the question and can pick and answer.
    
    Challenge: Make the question randomized

--- user.py
+++ user.py
@@ -1,11 +1,29 @@
 #make the questions
 questions = [
-("What school do I go to", "Northeastern"),("Who is the senior associate Dean of Khoury","Ben Hescott"),
-("What is my major", "computer science"), ("Where do I live", "New York")
+    {
+        "question": "What school do I go to?",
+        "choices": ["A. Harvard", "B. Princeston", "C. Northeastern", "D. IIT"],
+        "answer": "c"
+    },
+    {
+        "question": "Who is the senior associate Dean of Khoury?",
+        "choices": ["A. Ben Hescott", "B. Abhishek Pujara", "C. Alavaro Monge", "D. Elizabeth Mynatt"],
+        "answer": "a"
+    },
+    {
+        "question": "What is my major?",
+        "choices": ["A. Business", "B. Computer Science", "C. Applied Mathematics", "D. Gender Studies"],
+        "answer": "b"
+    },
+    {
+        "question": "Where do I live?",
+        "choices": ["A. A small hut in Ghana", "B. An hotel in Boston", "C. Presidential Suite at the Ritz", "D. A cave under the Artic desert"],
+        "answer": "b"
+    }
 ]
 #make a nice intro to the user
-name = input("What is your name? ")
-print(f"Hello, {name}")
+name = input("What is your first name ")
+print(f"What's up, {name}")
 # Prints out question
 # gives you choices of A,B,C,D (edge case can be uppercase or lower case and spacing does not matter either)
 def playQuiz(): 
@@ -15,15 +33,20 @@ def playQuiz():
     print("let's start with a little game:\n" )
     
     for question in questions: 
-        print(f"{questionNum} : {question[0]}") #Just push out the question 
-        answer = input("YOU PUT:" ).strip() #you gotta find out how to make it so that if they put 
+        print(f"{questionNum} : {question['question']}") #Just push out the question 
         #an answer weird - the program can still take it (like edge cases) 
+        #Same thing now just iterate through all the a-d choices they have
+        for choice in question['choices']:
+            print(choice)
         
-        if answer.lower() == question[1].lower():
+        answer = input("YOU PUT:" ).strip().lower() #you gotta find out how to make it so that if they put
+        
+        if answer == question['answer']:
             score = score + 1 #they got it right!
         else: 
             score = score #They got it wrong!
-            print("nu uh")
+            print("WRONG")
+            
         questionNum = questionNum + 1 #move to next question
         
     
